62b4c050549a2da5a8f5cdd7ec2ceeb964a1037a
1900804 <yiyunfu@softmobile.com.tw>
2022-05-02 22:29:38 +0800

    plusMinus: O(N)

--- LeetCode.playground/Pages/plusMinus.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/plusMinus.xcplaygroundpage/Contents.swift
@@ -4,10 +4,19 @@ import Foundation
 
 func plusMinus(arr: [Int]) {
     let count: Double = Double(arr.count)
-    let positives: Double = Double(arr.filter({ $0 > 0}).count)
-    let negatives: Double = Double(arr.filter({ $0 < 0 }).count)
-    let zeros: Double = Double(arr.filter({ $0 == 0 }).count)
-    print(String(format: "%.6f %.6f %.6f", positives/count, negatives/count, zeros/count))
+    var positives: Double = 0
+    var negatives: Double = 0
+    var zeros: Double = 0
+    for arranged in arr {
+        if arranged == 0 {
+            zeros += 1
+        } else if arranged > 0 {
+            positives += 1
+        } else if arranged < 0 {
+            negatives += 1
+        }
+    }
+    print(String(format: "%.6f\n%.6f\n%.6f", positives/count, negatives/count, zeros/count))
 }
 
 plusMinus(arr: [-4,3,-9,0,4,1])
