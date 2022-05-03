20f610e836394ab254df150063f365ca073485b5
1900804 <yiyunfu@softmobile.com.tw>
2022-05-03 13:24:49 +0800

    edit timeConversion

--- LeetCode.playground/Pages/timeConversion.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/timeConversion.xcplaygroundpage/Contents.swift
@@ -1,35 +1,12 @@
 import Foundation
 
-enum timePadding : String {
-    case AM, PM
-    //    static var all: [String] {
-    //        return [timePadding.AM.rawValue, timePadding.PM.rawValue]
-    //    }
-}
-
-func calculateTime(_ time: String) -> String {
-    var times = time.components(separatedBy: ":")
-    let gap = (Int(times.first ?? "") ?? 0) - 12
-    if gap >= 0 {
-        times[0] = String(gap)
-    }
-    
-    return times.joined(separator: ":")
-}
-
 func timeConversion(s: String) -> String {
     // Write your code here
-    let padding = s.suffix(2)
-    var times = s.prefix(8).components(separatedBy: ":")
-    let gap = (Int(times.first ?? "") ?? 0) - 12
-    if (gap >= 0 && padding == "AM") {
-        times[0] = String(format: "%02d", gap)
-    } else if (gap < 0 && padding == "PM") {
-        times[0] = String(format: "%02d", (Int(times[0]) ?? 0) + 12)
+    var res = (Int(s.prefix(2)) ?? 0) % 12
+    if s.suffix(2) == "PM" {
+        res += 12
     }
-        
-    return times.joined(separator: ":")
+    return String.init(format: "%02d%@", res, String(String(s.suffix(8)).prefix(6)))
 }
-
 
 timeConversion(s: "01:01:00PM")
