13d2260d092b3f0ffcb774d8c0a65e4206d1277e
1900804 <yiyunfu@softmobile.com.tw>
2022-05-03 13:12:32 +0800

    edit miniMaxSum

--- LeetCode.playground/Pages/miniMaxSum.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/miniMaxSum.xcplaygroundpage/Contents.swift
@@ -1,11 +1,17 @@
 import Foundation
 
-func miniMaxSum(arr: [Int]) -> [Int] {
+func miniMaxSum(arr: [Int]) {
     // Write your code here
-    let sorted = arr.sorted()
-    let min = sorted[0..<sorted.count-1].reduce(0, +)
-    let max = sorted[1..<sorted.count].reduce(0, +)
-    return [min, max]
+    
+//    let sorted = arr.sorted() // O(nlogn)
+//    let min = sorted[0..<sorted.count-1].reduce(0, +) // O(n)
+//    let max = sorted[1..<sorted.count].reduce(0, +) // O(n)
+//    print(min, max)
+    
+    let sum = arr.reduce(0, +) // O(n)
+    let min = sum - (arr.max() ?? 0) // O(n)
+    let max = sum - (arr.min() ?? 0) // O(n)
+    print(min,max)
 }
 
 miniMaxSum(arr: [1,3,5,7,9])
