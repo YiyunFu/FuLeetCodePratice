900a7eb60f167751d1253244d0afc29260028d6b
YiyunFu <fuyiyun851206@gmail.com>
2022-08-22 22:06:36 +0800

    fix romanToInt

--- LeetCode.playground/Pages/romanToInt.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/romanToInt.xcplaygroundpage/Contents.swift
@@ -1,37 +1,30 @@
-class Solution {
-    func romanToInt(_ s: String) -> Int {
-        let priorityMap: [String:Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
-        var maps: [String : [Int]] = [String : [Int]]()
-        
-        let characters: [String] = s.map({ String($0) })
-        for i in 0..<characters.count {
-            let character = characters[i]
-            let charPriority: Int = priorityMap[character] ?? 0
-            
-            if (i != characters.count-1) {
-                let nextChar = characters[i+1]
-                let nextCharPriority: Int = priorityMap[nextChar] ?? 0
-                if (nextCharPriority > charPriority) {
-                    if maps[character] == nil {
-                        maps[character] = []
-                    }
-                    maps[character]?.append(-1)
-                    continue
-                }
-            }
-            if maps[character] == nil {
-                maps[character] = []
-            }
-            maps[character]?.append(1)
+func romanToInt(_ s: String) -> Int {
+    /// Start from last digit
+    /// if current iterating digit is smaller than previous digit Substract the value from sum
+    /// otherwise ADD corresponding integer value of last digit to sum
+    
+    let priorityMap: [String:Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+    let arr = s.map({ String($0) })
+    var index = s.count-1
+    var sum = 0
+    var last = 0
+    
+    while index >= 0 {
+        guard let current = priorityMap[arr[index]] else {
+            return sum
         }
         
-        var result: Int = 0
-        for (key, value) in maps {
-            let count = value.reduce(0,+)
-            let res = count * (priorityMap[key] ?? 0)
-            result += res
+        if last > current && last > 0 {
+            sum -= current
+        } else {
+            sum += current
         }
         
-        return result
+        last = current
+        index -= 1
     }
+    
+    return sum
 }
+
+romanToInt("MCMXCIV")
